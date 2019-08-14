8696352faf3b3444d0606270e746efd8f03c5e37
Hardy Slartibartfast <hslartib@at-m2.21-school.ru>
2019-08-14 19:55:45 +0300

    =)

--- src/lem_strt.c
+++ src/lem_strt.c
@@ -10,53 +10,100 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./inc/lem_strt.h"
+#include "lem_strt.h"
 
+void	end_vertc(t_info *info, char *str)
+{
+	int i;
+	char **mas;
 
-//void len_vrtc(t_info *info, int index)
-//{
-//	int k;
-//
-//	k = 0;
-//	while (k < info->rooms[index]->links_len && (info->len_path[info->rooms[index]->links[k]] == 0 || info->len_path[index] + 1 < info->len_path[info->rooms[index]->links[k]]) && k != info->start)
-//	{
-//		info->len_path[k] = info->len_path[index] + 1;
-//		k++;
-//	}
-//	k = 0;
-//	while (k < info->rooms[index]->links_len && info->len_path[k] == 0 && k != info->start)
-//	{
-//		len_vrtc(info, k);
-//		k++;
-//	}
-//}
-//
-//void path(t_info *info, char *str, int index)
-//{
-//	int k;
-//
-//	k = 0;
-//	while (k < info->rooms[index]->links_len && (info->len_path[index] + 1 = info->len_path[info->rooms[index]->links[0]] || info->len_path[k] = 0))
-//	{
-//		info->len_path[k] =
-//		path(info, str, k);
-//		k++;
-//	}
-//}
-//
-//void workroom(t_info *info)
-//{
-//	int		i;
-//	char	*str;
-//
-//	str = ft_strnew(0);
-//	info->len_path = (int *)malloc(sizeof(int) * info->count_room);
-//	i = 0;
-//	while (i < info->count_room)
-//		info->len_path[i++] = 0;
-//	len_vrtc(info, info->start);
-//	path(info, str, info->start);
-//}
+	mas = ft_strsplit(str, '-');
+	i = 0;
+	while (mas[i])
+		i++;
+	info->count_end_vrt = i;
+	info->vrtc_end = (int *)malloc(sizeof(int) * i);
+	i = 0;
+	while (mas[i])
+	{
+		info->vrtc_end[i] = ft_atoi(mas[i]);
+		free(mas[i]);
+		i++;
+	}
+	free(mas);
+	free(str);
+}
+
+void len_vrtc(t_info *info, int index)
+{
+	int k;
+
+	k = 0;
+	while (k < info->rooms[index]->links_len && (info->len_path[info->rooms[index]->links[k]] == 0 || info->len_path[index] + 1 < info->len_path[info->rooms[index]->links[k]]) && k != info->start)
+	{
+		info->len_path[k] = info->len_path[index] + 1;
+		k++;
+	}
+	k = 0;
+	while (k < info->rooms[index]->links_len && info->len_path[k] == 0 && k != info->start)
+	{
+		len_vrtc(info, info->rooms[index]->links[k]);
+		k++;
+	}
+}
+
+void path(t_info *info, char *str, int index, char **vrtc_end)
+{
+	int k;
+	int i;
+	char *path_indx;
+
+	path_indx = info->rooms[index]->all_path;
+	k = 0;
+	i = 0;
+	if (*str)
+		str = ft_strjoin("-", str);
+	str = ft_strjoin(str, ft_itoa(index));
+	if (index != info->start && !(ft_strstr(path_indx, str)))
+	{
+		if (*str)
+			str = ft_strjoin("#", str);
+		path_indx = ft_strjoin(path_indx, str);
+	}
+	while (k < info->rooms[index]->links_len)
+	{
+		if (info->len_path[index] + 1 == info->len_path[info->rooms[index]->links[k]])
+		{
+			path(info, str, k);
+			i++;
+		}
+		k++;
+	}
+	if (!i)
+	{
+		if (**vrtc_end)
+			*vrtc_end = ft_strjoin("-", *vrtc_end);
+		*vrtc_end = ft_strjoin(*vrtc_end, ft_itoa(index));
+	}
+}
+
+void workroom(t_info *info)
+{
+	int		i;
+	char	*str;
+	char	*vrtc_end;
+
+	str = ft_strnew(0);
+	vrtc_end = ft_strnew(0);
+
+	info->len_path = (int *)malloc(sizeof(int) * info->count_room);
+	i = 0;
+	while (i < info->count_room)
+		info->len_path[i++] = 0;
+	len_vrtc(info, info->start);
+	path(info, str, info->start, &vrtc_end);
+	end_vertc(info, vrtc_end);
+}
 
 int main()
 {
@@ -64,7 +111,7 @@ int main()
 
 	if (!(info = (t_info *)malloc(sizeof(t_info))))
 		return (0);
-	info_init(info);
-//	workroom(info);
+	workroom(info);
+	work_len_finish(info);
 	return (0);
 }
---
+++ src/work_len_finish.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   work_len_finish.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hslartib <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/14 17:49:53 by hslartib          #+#    #+#             */
+/*   Updated: 2019/08/14 17:49:54 by hslartib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_strt.h"
+
+void		len_vrt_end(t_info *info, t_end *end, int index)
+{
+	int k;
+
+	k = 0;
+	while (k < info->rooms[index]->links_len)
+	{
+		if ((info->len_path[info->rooms[index]->links[k]] == 0 || info->len_path[index] + 1 < info->len_path[info->rooms[index]->links[k]]) && k != info->start)
+			end->len_path[info->rooms[index]->links[k]] = end->len_path[index] + 1;
+		k++;
+	}
+	k = 0;
+	while (k < info->rooms[index]->links_len && info->len_path[k] == 0 && k != info->start)
+	{
+		len_vrt_end(info, end, info->rooms[index]->links[k]);
+		k++;
+	}
+}
+
+void work_len_finish(t_info *info, t_end *end)
+{
+	int i;
+
+	info->len_path = (int *)malloc(sizeof(int) * info->count_room);
+	i = 0;
+	while (i < info->count_room)
+		info->len_path[i++] = 0;
+	len_vrt_end(info, info->start, info->end);
+}
